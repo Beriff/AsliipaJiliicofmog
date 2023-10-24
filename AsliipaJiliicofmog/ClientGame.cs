8fb650a30a5b2802693d65fe65a4a85fc5e9c932
Beriff <58127431+Beriff@users.noreply.github.com>
2023-10-24 15:36:21 +0700

    lol

--- AsliipaJiliicofmog/ClientGame.cs
+++ AsliipaJiliicofmog/ClientGame.cs
@@ -1,5 +1,4 @@
-﻿using Asliipa;
-using AsliipaJiliicofmog;
+﻿using AsliipaJiliicofmog;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -7,95 +6,39 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Asliipa
+namespace AsliipaJiliicofmog
 {
 	class Client : Game
 	{
 		public GraphicsDeviceManager Graphics;
 		public SpriteBatch SB;
-		public UIControl Control;
-		public Animator Animator;
-		public World MainWorld;
-		public static Random GameRandom;
-		public static int Seed = 888;
 		public Client()
 		{
 			Graphics = new GraphicsDeviceManager(this);
+			
+
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
 		}
 		protected override void Initialize()
 		{
 			Graphics.GraphicsDevice.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+			SB = new SpriteBatch(Graphics.GraphicsDevice);
 			base.Initialize();
 		}
 		protected override void LoadContent()
 		{
-			SB = new SpriteBatch(GraphicsDevice);
-			GameRandom = new();
-			Control = new(UIColorPalette.Default(), SB, Content.Load<SpriteFont>("mplus"), Window);
-			Animator = new();
-			Loader.Init(Content, SB);
-			MainWorld = new World(new WorldGenSettings(10));
-			//JEREMY = Chunk.FillChunk(Registry.Tiles["dirt"]);
-
-
-			/*noisetexture = new Texture2D(GraphicsDevice, 500, 500);
-			var noise = OctaveValueNoise.WorldNoise(GameRandom);
-			var data = new Color[500 * 500];
-			NumExtend.XY(500, 500, (x, y) =>
-			{
-				float val = noise.Noise(new Vector2(x,y));
-				Color col = Color.White;
-				if (val < .375f)
-					col = Color.Blue;
-				else if (val < .4f)
-					col = Color.Cyan;
-				else if (val < .45f)
-					col = Color.Beige;
-				else if (val < .7f)
-					col = Color.Green;
-				else if (val < .75f)
-					col = Color.Chocolate;
-				else
-					col = Color.White;
-				data[NumExtend.Flatten(x, y, 500)] = col;
-			});
-			noisetexture.SetData(data);*/
+			
 		}
 		protected override void Update(GameTime gameTime)
 		{
 			base.Update(gameTime);
-			Control.Update(gameTime);
-			Animator.Update();
-			if(Control.Input.GetState(Keys.D) == PressState.Pressed)
-			{
-				MainWorld.Camera += new Vector2(1, 0);
-			}
-			if (Control.Input.GetState(Keys.S) == PressState.Pressed)
-			{
-				MainWorld.Camera += new Vector2(0, 1);
-			}
-			if (Control.Input.GetState(Keys.A) == PressState.Pressed)
-			{
-				MainWorld.Camera += new Vector2(-1, 0);
-			}
-			if (Control.Input.GetState(Keys.W) == PressState.Pressed)
-			{
-				MainWorld.Camera += new Vector2(0, -1);
-			}
+			
 		}
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 			SB.Begin();
-			Control.Render(SB, gameTime);
-			MainWorld.Render(SB);
-			//SB.Draw(noisetexture, Vector2.Zero, Color.White);
-			SB.DrawString(Control.Font, 
-				$"Asliipa Build 0.0.46 \n{MathF.Round(1/(float)gameTime.ElapsedGameTime.TotalSeconds, 2)} fps \nCP {MainWorld.Camera}"
-				+ $" (CHUNK {MainWorld.Camera - new Vector2(MainWorld.Camera.X % Chunk.CHUNKSIZEPX, MainWorld.Camera.Y % Chunk.CHUNKSIZEPX)})", 
-				Vector2.Zero, Color.White);
 			SB.End();
 			
 			base.Draw(gameTime);
--- AsliipaJiliicofmog/Main.cs
+++ AsliipaJiliicofmog/Main.cs
@@ -1,4 +1,4 @@
-﻿using Asliipa;
+﻿using AsliipaJiliicofmog;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
