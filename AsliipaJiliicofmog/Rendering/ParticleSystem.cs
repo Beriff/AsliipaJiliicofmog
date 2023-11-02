863b841307e60b24a7badca97754f4fe139ba531
Froxwin <56168224+Froxwin@users.noreply.github.com>
2023-11-02 18:13:40 +0530

    owo

--- AsliipaJiliicofmog/Rendering/ParticleSystem.cs
+++ AsliipaJiliicofmog/Rendering/ParticleSystem.cs
@@ -7,74 +7,79 @@ using Microsoft.Xna.Framework.Graphics;
 namespace AsliipaJiliicofmog.Rendering
 {
 
-internal class Emitter
-{
-  List<Particle> particles = new();
-  Vector2 origin;
-  Texture2D texture;
+  internal class Emitter
+  {
+    List<Particle> particles = new();
+    Vector2 origin;
+    Texture2D texture;
 
-  Random r = new Random();
+    Random r = new Random();
 
-  public Emitter(Vector2 o, Texture2D t) {
-    origin = o;
-    texture = t;
-  }
-
-  public void Render(SpriteBatch sb, World w) {
-    foreach(Particle x in particles) x.Display(sb, w);
-    particles.Add(new Particle(origin, texture, new Vector2(0.01f, 0.01f), new Vector2((float)(((-1) ^ r.Next(0,3)) * r.NextDouble()),0)));
-  }
-
-  public void Update() {
-    List<Particle> temp = new();
-    foreach(Particle x in particles) {
-      x.Update();
-      if (!x.IsDead()) temp.Add(x);
+    public Emitter(Vector2 o, Texture2D t)
+    {
+      origin = o;
+      texture = t;
     }
-    particles = temp;
-  }
-}
 
-	internal class Particle
-	{
-		public Texture2D Texture;
+    public void Render(SpriteBatch sb, World w)
+    {
+      foreach (Particle x in particles) x.Display(sb, w);
+      particles.Add(new Particle(origin, texture, new Vector2(0.01f, 0.01f), new Vector2((float)(((-1) ^ r.Next(0, 3)) * r.NextDouble()), 0)));
+    }
 
-		public float Lifetime;
-		public float Lifespan;
-		public float ParticleLife;
-
-		public float Speed;
-		public Vector2 Position;
-		public Vector2 Velocity;
-		public Vector2 Acceleration;
-
-		public Emitter Emitter;
-		public float EmissionRate;
-
-		public Vector2 Scale;
-
-  public Particle(Vector2 l, Texture2D t, Vector2 s, Vector2 v)
-  {
-    Position = l;
-    Acceleration = new Vector2(0, 0.01f);
-    Velocity = v;
-    Lifespan = 255;
-    Texture = t;
-    Scale = s;
+    public void Update()
+    {
+      List<Particle> temp = new();
+      foreach (Particle x in particles)
+      {
+        x.Update();
+        if (!x.IsDead()) temp.Add(x);
+      }
+      particles = temp;
+    }
   }
 
-  public void Update()
+  internal class Particle
   {
-    Velocity += Acceleration;
-    Position += Velocity;
-    Lifespan -= 1;
-  }
+    public Texture2D Texture;
 
-  public bool IsDead() => Lifespan < 0;
+    public float Lifetime;
+    public float Lifespan;
+    public float ParticleLife;
 
-  public void Display(SpriteBatch sb, World w)
-  {
-    // sb.Draw(texture, w.GetWorldPosition(position, sb), new Color(255, 255, 255, lifespan));
-    sb.Draw(Texture, w.GetWorldPosition(Position, sb), null, new Color(255, 255, 255, Lifespan), 0f, new Vector2(0, 0), Scale, SpriteEffects.None, 0f);
+    public float Speed;
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public Vector2 Acceleration;
+
+    public Emitter Emitter;
+    public float EmissionRate;
+
+    public Vector2 Scale;
+
+    public Particle(Vector2 l, Texture2D t, Vector2 s, Vector2 v)
+    {
+      Position = l;
+      Acceleration = new Vector2(0, 0.01f);
+      Velocity = v;
+      Lifespan = 255;
+      Texture = t;
+      Scale = s;
+    }
+
+    public void Update()
+    {
+      Velocity += Acceleration;
+      Position += Velocity;
+      Lifespan -= 1;
+    }
+
+    public bool IsDead() => Lifespan < 0;
+
+    public void Display(SpriteBatch sb, World w)
+    {
+      // sb.Draw(texture, w.GetWorldPosition(position, sb), new Color(255, 255, 255, lifespan));
+      sb.Draw(Texture, w.GetWorldPosition(Position, sb), null, new Color(255, 255, 255, Lifespan), 0f, new Vector2(0, 0), Scale, SpriteEffects.None, 0f);
+    }
   }
 }
