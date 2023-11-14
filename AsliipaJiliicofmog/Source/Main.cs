18a3f60f7caebb8087ba0e78491398d85f61181f
Froxwin <56168224+Froxwin@users.noreply.github.com>
2023-11-14 20:34:16 +0530

    refactoring

--- AsliipaJiliicofmog/Source/Main.cs
+++ AsliipaJiliicofmog/Source/Main.cs
@@ -1,15 +1,2 @@
-﻿using AsliipaJiliicofmog;
-using System;
-
-namespace Asliipa
-{
-	class Program
-	{
-		[STAThread]
-		static void Main(string[] args)
-		{
-			using var game = new Client();
-			game.Run();
-		}
-	}
-}
+﻿using AsliipaJiliicofmog.Client game = new();
+game.Run();
