421d98fa6b72ac5b97c16b8fe3c6d88d5c4eccce
Beriff <58127431+Beriff@users.noreply.github.com>
2023-11-14 21:44:48 +0700

    Add test solution dependencies

---
+++ AsliipaJiliicofmog/Source/Env/Item/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsliipaJiliicofmog.Env.Item
+{
+	public class Item
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public bool Composite { get; set; }
+		public List<Item> ParentItems { get; set; }
+		public Material Material { get; set; }
+
+
+	}
+}
